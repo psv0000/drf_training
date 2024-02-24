d6ab7713e3d350ff3af8eeec2b14c66d9ca20c07
Sergei Plotnikov <plotnikov2895@gmail.com>
2024-02-24 14:19:54 +0500

    third commit

--- women/views.py
+++ women/views.py
@@ -1,6 +1,7 @@
 from django.db.migrations import serializer
 from django.forms import model_to_dict
 from django.shortcuts import render
+from django.template.base import kwarg_re
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -16,10 +17,31 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id'],
-        )
-        return Response({'post': WomenSerializer(post_new).data})
+        return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'error': 'Object does not exists'})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({'post': serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if not pk:
+            return Response({'error': 'Method DELETE not allowed'})
+
+        Women.objects.filter(pk=pk).delete()
+
+        return Response({'post': 'delete post ' + str(pk)})
