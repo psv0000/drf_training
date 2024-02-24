184f847512a157f8b0414e8193104e8ee6093ecb
Sergei Plotnikov <plotnikov2895@gmail.com>
2024-02-24 18:36:06 +0500

    session-based authentication

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from women.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/women/', WomenAPIList.as_view()),
     path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
